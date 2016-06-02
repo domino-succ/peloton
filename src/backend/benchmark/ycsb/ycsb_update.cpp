23f5ae79f20f8616bd81579ba13ed34ce74d9b5f
Michael-Tieying-Zhang <tieyingz@cs.cmu.edu>
2016-06-02 15:43:03 -0400

    retry method changed

--- src/backend/benchmark/ycsb/ycsb_update.cpp
+++ src/backend/benchmark/ycsb/ycsb_update.cpp
@@ -10,8 +10,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-
-
 #include <memory>
 #include <string>
 #include <unordered_map>
@@ -75,6 +73,241 @@
 namespace peloton {
 namespace benchmark {
 namespace ycsb {
+/////////////////////////////////////////////////////////////////////////////////////////
+// Queue Based YCSB methods
+/////////////////////////////////////////////////////////////////////////////////////////
+
+void GenerateAndQueueUpdate(ZipfDistribution &zipf) {
+
+  /////////////////////////////////////////////////////////
+  // Generate INDEX SCAN + PREDICATE
+  /////////////////////////////////////////////////////////
+  std::vector<oid_t> key_column_ids;
+  key_column_ids.push_back(0);
+
+  std::vector<ExpressionType> expr_types;
+  expr_types.push_back(ExpressionType::EXPRESSION_TYPE_COMPARE_EQUAL);
+
+  auto lookup_key = zipf.GetNextNumber();
+  std::vector<Value> values;
+  values.push_back(ValueFactory::GetIntegerValue(lookup_key));
+
+  std::vector<expression::AbstractExpression *> runtime_keys;
+  auto ycsb_pkey_index = user_table->GetIndexWithOid(user_table_pkey_index_oid);
+
+  planner::IndexScanPlan::IndexScanDesc index_scan_desc(
+      ycsb_pkey_index, key_column_ids, expr_types, values, runtime_keys);
+
+  // Create plan node.
+  auto predicate = nullptr;
+
+  // Column ids to be added to logical tile after scan.
+  std::vector<oid_t> column_ids;
+  oid_t column_count = state.column_count + 1;
+  for (oid_t col_itr = 0; col_itr < column_count; col_itr++) {
+    column_ids.push_back(col_itr);
+  }
+
+  // Create and set up index scan executor
+  planner::IndexScanPlan index_scan_node(user_table, predicate, column_ids,
+                                         index_scan_desc);
+
+  // TODO: Should delete after executing the query
+  executor::IndexScanExecutor *index_scan_executor =
+      new executor::IndexScanExecutor(&index_scan_node, nullptr);
+
+  /////////////////////////////////////////////////////////
+  // Generate UPDATE: plan , executor , query
+  /////////////////////////////////////////////////////////
+
+  TargetList target_list;
+  Value update_val = ValueFactory::GetIntegerValue(2);
+  target_list.emplace_back(
+      1, expression::ExpressionUtil::ConstantValueFactory(update_val));
+
+  DirectMapList direct_map_list;
+  // Update the second attribute
+  for (oid_t col_itr = 0; col_itr < column_count; col_itr++) {
+    if (col_itr != 1) {
+      direct_map_list.emplace_back(col_itr,
+                                   std::pair<oid_t, oid_t>(0, col_itr));
+    }
+  }
+
+  // Generate update plan
+  std::unique_ptr<const planner::ProjectInfo> project_info(
+      new planner::ProjectInfo(std::move(target_list),
+                               std::move(direct_map_list)));
+  planner::UpdatePlan *update_node =
+      new planner::UpdatePlan(user_table, std::move(project_info));
+
+  // Generate update executor
+  executor::UpdateExecutor *update_executor =
+      new executor::UpdateExecutor(update_node, nullptr);
+  update_executor->AddChild(index_scan_executor);
+  update_executor->Init();
+
+  // Generate update query
+  UpdateQuery *query =
+      new UpdateQuery(index_scan_executor, update_executor, update_node);
+
+  /////////////////////////////////////////////////////////
+  // Set context
+  /////////////////////////////////////////////////////////
+  // SetUpdate(query, zipf);
+  executor::ExecutorContext *context = new executor::ExecutorContext(nullptr);
+  query->SetContext(context);
+  query->ResetState();
+
+  //  std::vector<Value> values2;
+  //  auto lookup_key = zipf.GetNextNumber();
+  //  values2.push_back(ValueFactory::GetIntegerValue(lookup_key));
+  //  query->GetIndexScanExecutor()->SetValues(values2);
+
+  /////////////////////////////////////////////////////////
+  // Call txn scheduler to queue this executor
+  /////////////////////////////////////////////////////////
+
+  // Push the query into the queue
+  // Note: when poping the query and after executing it, the update_executor and
+  // index_executor should be deleted, then query itself should be deleted
+  concurrency::TransactionScheduler::GetInstance().SimpleEnqueue(query);
+
+  //  const planner::ProjectInfo *proj1 = update_node->GetProjectInfo();
+  //  const planner::ProjectInfo *proj2 =
+  //      reinterpret_cast<const planner::UpdatePlan *>(
+  //          update_executor->GetRawNode())->GetProjectInfo();
+  //  std::cout << "test proj1: " << proj1 << std::endl;
+  //  std::cout << "test proj2: " << proj2 << std::endl;
+}
+
+UpdateQuery *GenerateUpdate(ZipfDistribution &zipf) {
+
+  /////////////////////////////////////////////////////////
+  // Generate INDEX SCAN + PREDICATE
+  /////////////////////////////////////////////////////////
+  std::vector<oid_t> key_column_ids;
+  key_column_ids.push_back(0);
+
+  std::vector<ExpressionType> expr_types;
+  expr_types.push_back(ExpressionType::EXPRESSION_TYPE_COMPARE_EQUAL);
+
+  auto lookup_key = zipf.GetNextNumber();
+  std::vector<Value> values;
+  values.push_back(ValueFactory::GetIntegerValue(lookup_key));
+
+  std::vector<expression::AbstractExpression *> runtime_keys;
+  auto ycsb_pkey_index = user_table->GetIndexWithOid(user_table_pkey_index_oid);
+
+  planner::IndexScanPlan::IndexScanDesc index_scan_desc(
+      ycsb_pkey_index, key_column_ids, expr_types, values, runtime_keys);
+
+  // Create plan node.
+  auto predicate = nullptr;
+
+  // Column ids to be added to logical tile after scan.
+  std::vector<oid_t> column_ids;
+  oid_t column_count = state.column_count + 1;
+  for (oid_t col_itr = 0; col_itr < column_count; col_itr++) {
+    column_ids.push_back(col_itr);
+  }
+
+  // Create and set up index scan executor
+  planner::IndexScanPlan index_scan_node(user_table, predicate, column_ids,
+                                         index_scan_desc);
+
+  // TODO: Should delete after executing the query
+  executor::IndexScanExecutor *index_scan_executor =
+      new executor::IndexScanExecutor(&index_scan_node, nullptr);
+
+  /////////////////////////////////////////////////////////
+  // Generate UPDATE: plan , executor , query
+  /////////////////////////////////////////////////////////
+
+  TargetList target_list;
+  Value update_val = ValueFactory::GetIntegerValue(2);
+  target_list.emplace_back(
+      1, expression::ExpressionUtil::ConstantValueFactory(update_val));
+
+  DirectMapList direct_map_list;
+  // Update the second attribute
+  for (oid_t col_itr = 0; col_itr < column_count; col_itr++) {
+    if (col_itr != 1) {
+      direct_map_list.emplace_back(col_itr,
+                                   std::pair<oid_t, oid_t>(0, col_itr));
+    }
+  }
+
+  // Generate update plan
+  std::unique_ptr<const planner::ProjectInfo> project_info(
+      new planner::ProjectInfo(std::move(target_list),
+                               std::move(direct_map_list)));
+  planner::UpdatePlan *update_node =
+      new planner::UpdatePlan(user_table, std::move(project_info));
+
+  // Generate update executor
+  executor::UpdateExecutor *update_executor =
+      new executor::UpdateExecutor(update_node, nullptr);
+  update_executor->AddChild(index_scan_executor);
+  update_executor->Init();
+
+  // Generate update query
+  UpdateQuery *query =
+      new UpdateQuery(index_scan_executor, update_executor, update_node);
+
+  /////////////////////////////////////////////////////////
+  // Set context
+  /////////////////////////////////////////////////////////
+  // SetUpdate(query, zipf);
+  executor::ExecutorContext *context = new executor::ExecutorContext(nullptr);
+  query->SetContext(context);
+  query->ResetState();
+
+  return query;
+}
+
+bool ExecuteUpdate(UpdateQuery *query) {
+  // Start a txn to execute the query
+  auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
+  auto txn = txn_manager.BeginTransaction();
+
+  ExecuteUpdateTest(query->GetUpdateExecutor());
+
+  /////////////////////////////////////////////////////////
+  // Transaction fail
+  /////////////////////////////////////////////////////////
+  if (txn->GetResult() != Result::RESULT_SUCCESS) {
+    txn_manager.AbortTransaction();
+    return false;
+  }
+
+  /////////////////////////////////////////////////////////
+  // Transaction success
+  /////////////////////////////////////////////////////////
+  assert(txn->GetResult() == Result::RESULT_SUCCESS);
+  auto result = txn_manager.CommitTransaction();
+
+  if (result == Result::RESULT_SUCCESS) {
+    return true;
+  } else {
+    // transaction failed commitment.
+    assert(result == Result::RESULT_ABORTED ||
+           result == Result::RESULT_FAILURE);
+    return false;
+  }
+}
+
+// If return true and query is null, that means the queue is empty
+// If return true and query is not null, that means execute successfully
+// If return false, that means execute fail
+bool PopAndExecuteUpdate(UpdateQuery *&ret_query) {
+  // Get a query from a queue
+  concurrency::TransactionQuery *query = nullptr;
+  concurrency::TransactionScheduler::GetInstance().SimpleDequeue(query);
+
+  ret_query = reinterpret_cast<UpdateQuery *>(query);
+  return ExecuteUpdate(ret_query);
+}
 
 UpdatePlans PrepareUpdatePlan() {
 
@@ -90,7 +323,7 @@ UpdatePlans PrepareUpdatePlan() {
   std::vector<Value> values;
 
   std::vector<expression::AbstractExpression *> runtime_keys;
-  
+
   auto ycsb_pkey_index = user_table->GetIndexWithOid(user_table_pkey_index_oid);
 
   planner::IndexScanPlan::IndexScanDesc index_scan_desc(
@@ -108,9 +341,9 @@ UpdatePlans PrepareUpdatePlan() {
   }
 
   // Create and set up index scan executor
-  planner::IndexScanPlan index_scan_node(
-      user_table, predicate, column_ids, index_scan_desc);
-  
+  planner::IndexScanPlan index_scan_node(user_table, predicate, column_ids,
+                                         index_scan_desc);
+
   executor::IndexScanExecutor *index_scan_executor =
       new executor::IndexScanExecutor(&index_scan_node, nullptr);
 
@@ -134,7 +367,7 @@ UpdatePlans PrepareUpdatePlan() {
                                std::move(direct_map_list)));
   planner::UpdatePlan update_node(user_table, std::move(project_info));
 
-  executor::UpdateExecutor *update_executor = 
+  executor::UpdateExecutor *update_executor =
       new executor::UpdateExecutor(&update_node, nullptr);
 
   update_executor->AddChild(index_scan_executor);
@@ -144,15 +377,14 @@ UpdatePlans PrepareUpdatePlan() {
   UpdatePlans update_plans;
 
   update_plans.index_scan_executor_ = index_scan_executor;
-  
+
   update_plans.update_executor_ = update_executor;
 
   return update_plans;
 }
 
-
 bool RunUpdate(UpdatePlans &update_plans, ZipfDistribution &zipf) {
-  
+
   std::unique_ptr<executor::ExecutorContext> context(
       new executor::ExecutorContext(nullptr));
 
@@ -171,18 +403,16 @@ bool RunUpdate(UpdatePlans &update_plans, ZipfDistribution &zipf) {
 
   update_plans.index_scan_executor_->SetValues(values);
 
-
   TargetList target_list;
   // std::string update_raw_value(ycsb_field_length - 1, 'u');
   int update_raw_value = 2;
-  
+
   Value update_val = ValueFactory::GetIntegerValue(update_raw_value);
 
   target_list.emplace_back(
       1, expression::ExpressionUtil::ConstantValueFactory(update_val));
 
   update_plans.update_executor_->SetTargetList(target_list);
-
 
   /////////////////////////////////////////////////////////
   // EXECUTE
@@ -202,7 +432,7 @@ bool RunUpdate(UpdatePlans &update_plans, ZipfDistribution &zipf) {
 
   if (result == Result::RESULT_SUCCESS) {
     return true;
-    
+
   } else {
     // transaction failed commitment.
     assert(result == Result::RESULT_ABORTED ||
@@ -210,7 +440,6 @@ bool RunUpdate(UpdatePlans &update_plans, ZipfDistribution &zipf) {
     return false;
   }
 }
-
 }
 }
 }
