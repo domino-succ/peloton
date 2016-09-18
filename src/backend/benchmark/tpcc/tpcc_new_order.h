45899c684edccf61a5dd91b91d6f677fef8dae41
Michael-Tieying-Zhang <tieyingz@cs.cmu.edu>
2016-09-18 15:59:52 -0400

    debug

--- src/backend/benchmark/tpcc/tpcc_new_order.h
+++ src/backend/benchmark/tpcc/tpcc_new_order.h
@@ -1954,9 +1954,10 @@ class NewOrder : public concurrency::TransactionQuery {
         }
       }
 
-      //      // For debug
-      //      std::cout << "max_conflict_key: " << max_conflict_key
-      //                << "---max_conflict:" << max_conflict << std::endl;
+      // For debug
+      std::cout << "Queue:" << queue_no
+                << ". max_conflict_key: " << max_conflict_key
+                << "---max_conflict:" << max_conflict << std::endl;
     }
     // If not canonical
     else {
