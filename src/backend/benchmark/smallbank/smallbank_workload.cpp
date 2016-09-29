4181eedf40ba7d911aefb2e143c010605fd2ebd0
Michael-Tieying-Zhang <tieyingz@cs.cmu.edu>
2016-09-28 22:08:45 -0400

    add exe time for tpcc

--- src/backend/benchmark/smallbank/smallbank_workload.cpp
+++ src/backend/benchmark/smallbank/smallbank_workload.cpp
@@ -471,13 +471,13 @@ void RunBackend(oid_t thread_id) {
               concurrency::TransactionScheduler::GetInstance().PartitionDequeue(
                   ret_query, thread_id);
         }
-        ret_pop =
-            concurrency::TransactionScheduler::GetInstance().PartitionDequeue(
-                ret_query, thread_id);
-
         //        ret_pop =
-        //            concurrency::TransactionScheduler::GetInstance().SimpleDequeue(
+        //            concurrency::TransactionScheduler::GetInstance().PartitionDequeue(
         //                ret_query, thread_id);
+
+        ret_pop =
+            concurrency::TransactionScheduler::GetInstance().SimpleDequeue(
+                ret_query, thread_id);
 
         break;
       }
@@ -488,8 +488,11 @@ void RunBackend(oid_t thread_id) {
         break;
       }
       case SCHEDULER_TYPE_CLUSTER: {
+        //        ret_pop =
+        //            concurrency::TransactionScheduler::GetInstance().PartitionDequeue(
+        //                ret_query, thread_id);
         ret_pop =
-            concurrency::TransactionScheduler::GetInstance().PartitionDequeue(
+            concurrency::TransactionScheduler::GetInstance().SimpleDequeue(
                 ret_query, thread_id);
         break;
       }
