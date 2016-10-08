e9d2f1f657f088a3a0ab5ed390f4b4765e0f9e74
Michael-Tieying-Zhang <tieyingz@cs.cmu.edu>
2016-10-08 17:53:23 -0400

    change parameters

--- src/backend/benchmark/tatp/tatp_workload.cpp
+++ src/backend/benchmark/tatp/tatp_workload.cpp
@@ -164,8 +164,8 @@ void GenerateAndCacheQuery(ZipfDistribution &zipf) {
   //
   // InsertCallForwarding *txn = GenerateInsertCallForwarding(zipf);
   // DeteteCallForwarding *txn = GenerateDeteteCallForwarding(zipf);
-  // UpdateSubscriberData *txn = GenerateUpdateSubscriberData(zipf);  // 5176
-  TestUpdateLocation *txn = GenerateTestUpdateLocation(zipf);
+  UpdateSubscriberData *txn = GenerateUpdateSubscriberData(zipf);  // 5176
+  // TestUpdateLocation *txn = GenerateTestUpdateLocation(zipf);
   // UpdateLocation *txn = GenerateUpdateLocation(zipf);  // 5303
   /////////////////////////////////////////////////////////
   // Call txn scheduler to queue this executor
@@ -226,8 +226,8 @@ void GenerateALLAndCache(ZipfDistribution &zipf) {
   }
   // UPDATE_LOCATION
   else {
-    // UpdateLocation *txn = GenerateUpdateLocation(zipf);
-    TestUpdateLocation *txn = GenerateTestUpdateLocation(zipf);
+    UpdateLocation *txn = GenerateUpdateLocation(zipf);
+    // TestUpdateLocation *txn = GenerateTestUpdateLocation(zipf);
     concurrency::TransactionScheduler::GetInstance().CacheQuery(txn);
   }
 }
